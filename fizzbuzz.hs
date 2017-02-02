2a1bf4ac862a96e72e94347fa5de0e5d60d6f75e
mazinai-tarazu <mazinai.tarazu@gmail.com>
2017-02-02 16:54:00 +0900

    Update fizzbuzz

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -2,12 +2,10 @@ main = do
   let f = fizzbuzz 15
   print f
 
-fizzbuzz num = answer
-  where
+fizzbuzz num = map (tlanslate) [1..num] where
     tlanslate n
       | mod n 15 == 0 = "FizzBuzz"
       | mod n 5 == 0 = "Buzz"
       | mod n 3 == 0 = "Fizz"
       | otherwise = show n
 
-    answer = map (tlanslate) [1..num]
